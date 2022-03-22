b5190e5a9aae5f856565c2d74d7502fcb41bc55a
Sungho Wi <53851699+bruno-wi@users.noreply.github.com>
2022-03-22 21:23:45 +0900

    Update 1158_0322_sh.kt

---
+++ DataStructure/0322/1158_0322_sh.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val (n, k) = System.`in`.bufferedReader().readLine().split(" ").map { it.toInt() }
+    val circleList = (1..n).toMutableList()
+    var currentIndex = 0
+
+    val resultList = mutableListOf<Int>()
+
+    while (circleList.size > 0) {
+        for (i in (1 until k)) {
+            currentIndex += 1
+            if (circleList.size <= currentIndex) {
+                currentIndex %= circleList.size
+            }
+        }
+        resultList.add(circleList[currentIndex])
+        circleList.removeAt(currentIndex)
+    }
+
+    print(resultList.joinToString(separator = ", ", prefix = "<", postfix = ">"))
+}
