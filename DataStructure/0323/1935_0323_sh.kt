8c342bf2057212070c4a366e169ac8de65976820
Sungho Wi <53851699+bruno-wi@users.noreply.github.com>
2022-03-23 14:32:06 +0900

    자료구조 : 후위 표기식2

---
+++ DataStructure/0323/1935_0323_sh.kt
@@ -0,0 +1,39 @@
+import java.text.DecimalFormat
+
+fun main() {
+    val numberMap = mutableMapOf<Char, Double>()
+    val formula = mutableListOf<Char>()
+
+    with(System.`in`.bufferedReader()) {
+        val n = readLine().toInt()
+        formula.addAll(readLine().toList())
+        var alphabet = 'A'
+        for (i in 0 until n) {
+            numberMap[alphabet++] = readLine().toDouble()
+        }
+    }
+
+    val calculationStack = mutableListOf<Double>()
+
+    for (c in formula) {
+        if (c in 'A'..'Z') {
+            calculationStack.add(numberMap[c]!!)
+        } else {
+            val last = calculationStack.removeLast().toDouble()
+            val first = calculationStack.removeLast().toDouble()
+
+            val result = when (c) {
+                '*' -> first * last
+                '+' -> first + last
+                '-' -> first - last
+                '/' -> first / last
+                '-' -> first - last
+                else -> 0.0
+            }
+            calculationStack.add(result)
+        }
+
+    }
+
+    print(DecimalFormat("#.00").format(calculationStack.last()))
+}
