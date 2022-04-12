9b40b43445483f977b799e2725223a66c8cd8dac
waterglasses <hsk10271@gmail.com>
2022-04-12 15:13:53 +0900

    그래프: 바이러스

---
+++ Graph/0422/2606_0412_sk.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString().trim()
+    : `7
+6
+1 2
+2 3
+1 5
+5 2
+5 6
+4 7`
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const getCntComputerWithVirus = (numOfComputer) => {
+  const visitedComputer = new Array(numOfCmputers + 1).fill(false);
+  const stackNodeList = [numOfComputer];
+  let countOfVirusComputer = 0;
+
+  while (stackNodeList.length) {
+    let computer = stackNodeList.pop();
+
+    if (visitedComputer[computer]) continue;
+    visitedComputer[computer] = true;
+
+    countOfVirusComputer += 1;
+
+    for (let connectedComputer of graphOfComputers[computer]) {
+      stackNodeList.push(connectedComputer);
+    }
+  }
+
+  return countOfVirusComputer - 1;
+};
+
+const numOfCmputers = parseInt(input());
+const pairOfComputers = parseInt(input());
+const graphOfComputers = Array.from(
+  new Array(numOfCmputers + 1),
+  () => new Array()
+);
+for (let i = 1; i <= pairOfComputers; i++) {
+  const [vertexA, vertexB] = input().split(" ").map(Number);
+
+  graphOfComputers[vertexA].push(vertexB);
+  graphOfComputers[vertexB].push(vertexA);
+}
+
+console.log(getCntComputerWithVirus(1));
